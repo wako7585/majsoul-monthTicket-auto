10b6d1d37fa004294dfaac7c3e9daeed6c9bc456
Dondonzzz <154062292+wako7585@users.noreply.github.com>
2024-08-24 12:02:05 +0800

    Upd for CN version

--- main.py
+++ main.py
@@ -1,70 +1,65 @@
 import asyncio
-import logging
-import random
-import uuid
+import hashlib
+import hmac
 import json
-import aiohttp
+import logging
 import os
+import random
+import time
+import uuid
+
+import aiohttp
 
 from ms.base import MSRPCChannel
 from ms.rpc import Lobby
 import ms.protocol_pb2 as pb
+from google.protobuf.json_format import MessageToJson
 
-uid = os.getenv("UID", "default_uid")
-token = os.getenv("TOKEN", "default_token")
-
-deviceId = f"web|{uid}"
-
-MS_HOST = "https://game.mahjongsoul.com/"
-PASSPORT_HOST = "https://passport.mahjongsoul.com/"
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
+MS_HOST = "https://game.maj-soul.com"
+
 
 async def main():
+    """
+    Login to the CN server with username and password and get latest 30 game logs.
+    """
 
+    username = os.getenv("USERNAME", "default_username")
+    password = os.getenv("PASSWORD", "default_password")
 
-    lobby, channel, version_to_force, accessTokenFromPassport = await connect()
-    await login(lobby, version_to_force, accessTokenFromPassport)
+    if not username or not password:
+        logging.info("Username or password cant be empty")
+        return
+
+    lobby, channel, version_to_force = await connect()
+    await login(lobby, username, password, version_to_force)
 
     await channel.close()
 
 
 async def connect():
     async with aiohttp.ClientSession() as session:
-        async with session.get("{}version.json".format(MS_HOST)) as res:
+        async with session.get("{}/1/version.json".format(MS_HOST)) as res:
             version = await res.json()
             logging.info(f"Version: {version}")
             version = version["version"]
             version_to_force = version.replace(".w", "")
 
-        async with session.get("{}v{}/config.json".format(MS_HOST, version)) as res:
+        async with session.get("{}/1/v{}/config.json".format(MS_HOST, version)) as res:
             config = await res.json()
             logging.info(f"Config: {config}")
 
-            url = config["ip"][0]["region_urls"][0]["url"]
-            passport_url = config["yo_service_url"][0]
-            print(passport_url)
+            url = config["ip"][0]["region_urls"][1]["url"]
 
         async with session.get(url + "?service=ws-gateway&protocol=ws&ssl=true") as res:
             servers = await res.json()
-            # mjjpgs.mahjongsoul.com:9663
             logging.info(f"Available servers: {servers}")
 
             servers = servers["servers"]
             server = random.choice(servers)
             endpoint = "wss://{}/gateway".format(server)
-
-        async with session.post(
-            passport_url + "/user/login/",
-            data={
-                "uid": uid,
-                "token": token,
-                "deviceId": deviceId,
-            },
-        ) as res:
-            passport = await res.json()
-            accessTokenFromPassport = passport["accessToken"]
 
     logging.info(f"Chosen endpoint: {endpoint}")
     channel = MSRPCChannel(endpoint)
@@ -74,38 +69,29 @@ async def connect():
     await channel.connect(MS_HOST)
     logging.info("Connection was established")
 
-    return lobby, channel, version_to_force, accessTokenFromPassport
+    return lobby, channel, version_to_force
 
 
-async def login(lobby, version_to_force, accessTokenFromPassport):
+async def login(lobby, username, password, version_to_force):
     logging.info("Login with username and password")
 
-    heartBeat = pb.ReqHeatBeat()
-    heartBeat.no_operation_counter = 1
-    hbRes = await lobby.heatbeat(heartBeat)  # heartbeat는 로그인하기 전에 임의적으로 몇번 통신함
+    uuid_key = str(uuid.uuid1())
 
-    reqFromSoulLess = pb.ReqOauth2Auth()
-    reqFromSoulLess.type = 7
-    reqFromSoulLess.code = accessTokenFromPassport
-    reqFromSoulLess.uid = uid
-    reqFromSoulLess.client_version_string = f"web-{version_to_force}"
+    req = pb.ReqLogin()
+    req.account = username
+    req.password = hmac.new(b"lailai", password.encode(), hashlib.sha256).hexdigest()
+    req.device.is_browser = True
+    req.random_key = uuid_key
+    req.gen_access_token = True
+    req.client_version_string = f"web-{version_to_force}"
+    req.currency_platforms.append(2)
 
-    res = await lobby.oauth2_auth(reqFromSoulLess)
-
+    res = await lobby.login(req)
     token = res.access_token
     if not token:
         logging.error("Login Error:")
         logging.error(res)
         return False
-
-    # reqOauth2Check = pb.ReqOauth2Check()
-    # reqOauth2Check.type = 7
-    # reqOauth2Check.access_token = token
-    # resOauth2Check = await lobby.oauth2_check(reqOauth2Check)
-    # print(resOauth2Check)
-    # if not resOauth2Check.has_account:
-    #     print("Invalid access token")
-    #     return False
 
     reqOauth2Login = pb.ReqOauth2Login()
     reqOauth2Login.type = 7
@@ -123,15 +109,83 @@ async def login(lobby, version_to_force, accessTokenFromPassport):
 
     reqPayMonthTicket = pb.ReqCommon()
     resPayMonthTicket = await lobby.pay_month_ticket(reqPayMonthTicket)
-
-    print(resPayMonthTicket)
+    logging.info("resPayMonthTicket {}".format(resPayMonthTicket))
 
     reqFetchMonthTicketInfo = pb.ReqCommon()
     resFetchMonthTicketInfo = await lobby.fetch_month_ticket_info(reqFetchMonthTicketInfo)
-
-    print(resFetchMonthTicketInfo)
+    logging.info("resFetchMonthTicketInfo {}".format(resFetchMonthTicketInfo))
 
     return True
+
+
+async def load_game_logs(lobby):
+    logging.info("Loading game logs")
+
+    records = []
+    current = 1
+    step = 30
+    req = pb.ReqGameRecordList()
+    req.start = current
+    req.count = step
+    res = await lobby.fetch_game_record_list(req)
+    records.extend([r.uuid for r in res.record_list])
+
+    return records
+
+
+async def load_and_process_game_log(lobby, uuid, version_to_force):
+    logging.info("Loading game log")
+
+    req = pb.ReqGameRecord()
+    req.game_uuid = uuid
+    req.client_version_string = f"web-{version_to_force}"
+    res = await lobby.fetch_game_record(req)
+
+    record_wrapper = pb.Wrapper()
+    record_wrapper.ParseFromString(res.data)
+
+    game_details = pb.GameDetailRecords()
+    game_details.ParseFromString(record_wrapper.data)
+
+    game_records_count = len(game_details.records)
+    logging.info("Found {} game records".format(game_records_count))
+
+    round_record_wrapper = pb.Wrapper()
+    is_show_new_round_record = False
+    is_show_discard_tile = False
+    is_show_deal_tile = False
+
+    for i in range(0, game_records_count):
+        round_record_wrapper.ParseFromString(game_details.records[i])
+
+        if round_record_wrapper.name == ".lq.RecordNewRound" and not is_show_new_round_record:
+            logging.info("Found record type = {}".format(round_record_wrapper.name))
+            round_data = pb.RecordNewRound()
+            round_data.ParseFromString(round_record_wrapper.data)
+            print_data_as_json(round_data, "RecordNewRound")
+            is_show_new_round_record = True
+
+        if round_record_wrapper.name == ".lq.RecordDiscardTile" and not is_show_discard_tile:
+            logging.info("Found record type = {}".format(round_record_wrapper.name))
+            discard_tile = pb.RecordDiscardTile()
+            discard_tile.ParseFromString(round_record_wrapper.data)
+            print_data_as_json(discard_tile, "RecordDiscardTile")
+            is_show_discard_tile = True
+
+        if round_record_wrapper.name == ".lq.RecordDealTile" and not is_show_deal_tile:
+            logging.info("Found record type = {}".format(round_record_wrapper.name))
+            deal_tile = pb.RecordDealTile()
+            deal_tile.ParseFromString(round_record_wrapper.data)
+            print_data_as_json(deal_tile, "RecordDealTile")
+            is_show_deal_tile = True
+
+    return res
+
+
+def print_data_as_json(data, type):
+    json = MessageToJson(data)
+    logging.info("{} json {}".format(type, json))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
